55a3be53619c4e5639208a6296506b5d00bcced4
Oyewo Temidayo <oyewodayo@gmail.com>
2025-10-12 01:30:46 -0700

    feat(backend): add recording completion modal view
    - Create HTML view for recording completion notification
    - Add TypeScript component for modal interaction
    - Support file path display and quick access

---
+++ src-tauri/src/views/completed_recording.tsx
@@ -0,0 +1,10 @@
+// src/modal.js
+import React from 'react';
+import ReactDOM from "react-dom/client";
+
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+     {/* <FileModal */}
+    </React.StrictMode>,
+  );
